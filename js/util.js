a3abbf536a12c402f6e864f9299d26185cf6c766
frozyaC <kniga701@gmail.com>
2023-04-20 20:37:12 +0500

    fixed .js files

--- js/util.js
+++ js/util.js
@@ -1,4 +1,5 @@
-import './data.js';
+/* eslint-disable no-console */
+import { COMMENTS_LIST, NAMES } from './data.js';
 // function getRandomPositiveInteger(a, b) {
 //     const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
 //     const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
@@ -16,12 +17,14 @@ import './data.js';
 //   function checkStringLength(string, length) {
 //     return string.length <= length;
 //   }
+let globalId = -1;
+let commentId = -1;
 
 function randomNumber(a, b) {
   if (a > b || a === b) {
     return console.error('Wrong randomNumber parameters');
   }
-  return (Math.floor(Math.random() * b) + a);
+  return (Math.floor(Math.random() * b) + a + 1);
 }
 
 function commentsGenerator() {
@@ -38,12 +41,12 @@ const photoDescription = () => {
   globalId += 1;
   return {
     id: globalId,
-    url: `photos/${  globalId  }.jpg`,
+    url: `photos/${globalId}.jpg`,
     description: 'Лучшая фотография в своем роде',
     likes: randomNumber(15, 201),
     comments: commentsGenerator(),
   };
 };
 const generatePosts = Array.from({ length: 25 }, photoDescription);
-// eslint-disable-next-line no-console
+
 console.log(generatePosts);
